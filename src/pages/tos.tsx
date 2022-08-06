eb29ba24d202b414cbc3552579fb05587b88cd30
MinhHieu89 <minhhieu.sn89@gmail.com>
2022-08-06 12:45:14 +0700

    Added a layout for public pages.

--- src/pages/tos.tsx
+++ src/pages/tos.tsx
@@ -1,7 +1,11 @@
-import { NextPage } from 'next';
+import { PublicLayout } from '../components/layout';
 
-const TermAndCondition: NextPage = () => {
+const TermAndCondition: Page = () => {
 	return <div>Term And Condition</div>;
+};
+
+TermAndCondition.getLayout = (page: React.ReactNode) => {
+	return <PublicLayout>{page}</PublicLayout>;
 };
 
 export default TermAndCondition;
